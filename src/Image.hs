aa28083d6cc81e7486103773f8f764f07d466d1e
lbodor <lazar.bodor@gmail.com>
2015-07-12 12:02:33 +0000

    Make launch wait for instance and print its IP.

--- src/Image.hs
+++ src/Image.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Image
@@ -6,17 +7,19 @@ module Image
 
 import           Control.Arrow
 import           Control.Lens
+import           Control.Monad.Trans
 import           Control.Monad.Trans.AWS
 import           Data.Function           (on)
 import           Data.List
 import           Data.Text (Text)
 import qualified Network.AWS.EC2         as EC2
+import           Safe
 
-latestImageByName :: Text -> AWST IO Text
+latestImageByName :: Text -> AWST IO (Maybe Text)
 latestImageByName name = do
     images <- view EC2.dirImages <$> send req
     let pairs = map (view EC2.iImageId &&& view EC2.iCreationDate) images
-    return $ fst $ head $ sortByDescendingDate pairs
+    return $ fst <$> headMay (sortByDescendingDate pairs)
   where
     req = EC2.describeImages
         & EC2.di2Owners  .~ ["self"]
@@ -26,14 +29,28 @@ latestImageByName name = do
 
 launch :: Text -> AWST IO ()
 launch name = do
-    imageId   <- latestImageByName name
-    instances <- view EC2.rirInstances <$> send (runInstanceRq imageId)
-    send_ $ createTagsRq $ map (view EC2.i1InstanceId) instances
+    latestImageByName name >>= maybe
+        (do liftIO $ putStrLn "image not found"
+            return ())
+        (\imageId -> do
+            instances <- view EC2.rirInstances <$> send (runInstanceRq imageId)
+            let instanceIds = map (^. EC2.i1InstanceId) instances
+            assignName instanceIds name
+            reservations <- view EC2.dirReservations <$> await EC2.instanceRunning (describeInstancesRq instanceIds)
+            let runningInstances = concatMap (^. EC2.rInstances) reservations
+            liftIO $ print $ map (^. EC2.i1PublicIpAddress) runningInstances
+            return ())
+            
   where
     runInstanceRq imageId = EC2.runInstances imageId 1 1
         & EC2.riInstanceType ?~ EC2.T2_Medium
 
-    createTagsRq instanceIds = EC2.createTags
-        & EC2.ct1Resources .~ instanceIds
+    describeInstancesRq instanceIds = EC2.describeInstances
+        & EC2.di1InstanceIds .~ instanceIds
+
+assignName :: [Text] -> Text -> AWST IO ()
+assignName resourceIds name =
+    send_ $ EC2.createTags
+        & EC2.ct1Resources .~ resourceIds
         & EC2.ct1Tags .~ [(EC2.tag "Name" name)]
 
