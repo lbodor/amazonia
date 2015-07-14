0c1cfee4c1b49bf1c689f7b83a6cc5b45b625be1
lbodor <lazar.bodor@gmail.com>
2015-07-14 12:07:21 +0000

    Switch to ExceptT.

--- src/Image.hs
+++ src/Image.hs
@@ -1,29 +1,36 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Image
     ( createImage
+    , latestImageByName
     , launch
     ) where
 
 import           Control.Arrow
 import           Control.Lens
-import           Control.Monad.Trans
+import           Control.Monad.Except
+import           Control.Monad.Morph
+-- import           Control.Monad.Trans
 import           Control.Monad.Trans.AWS
 import           Data.Function           (on)
 import           Data.List
+import           Data.Maybe
 import           Data.Monoid
 import           Data.Text (Text)
 import qualified Data.Text as T
 import           Data.Time.Clock.POSIX (getPOSIXTime)
 import qualified Network.AWS.EC2         as EC2
-import           Safe
+-- import           Safe
 
-latestImageByName :: Text -> AWST IO (Maybe Text)
+latestImageByName :: Text -> AWST (ExceptT Text IO) Text
 latestImageByName name = do
     images <- view EC2.dirImages <$> send req
     let pairs = map (view EC2.iImageId &&& view EC2.iCreationDate) images
-    return $ fst <$> headMay (sortByDescendingDate pairs)
+    case sortByDescendingDate pairs of
+        []    -> lift (throwError "no such image")
+        (p:_) -> return (fst p)
   where
     req = EC2.describeImages
         & EC2.di2Owners  .~ ["self"]
@@ -31,31 +38,41 @@ latestImageByName name = do
     filters = EC2.filter' "tag:Name" & EC2.fValues .~ [name]
     sortByDescendingDate = sortBy (flip compare `on` snd)
 
-launch :: Text -> AWST IO ()
+launch :: Text -> AWST (ExceptT Text IO) Text
 launch name = do
-    latestImageByName name >>= maybe
-        (do liftIO $ putStrLn "image not found"
-            return ())
-        (\imageId -> do
-            instances <- view EC2.rirInstances <$> send (runInstanceRq imageId)
-            let instanceIds = map (^. EC2.i1InstanceId) instances
-            assignName instanceIds name
-            reservations <- view EC2.dirReservations <$> await EC2.instanceRunning (describeInstancesRq instanceIds)
-            let runningInstances = concatMap (^. EC2.rInstances) reservations
-            liftIO $ print $ map (^. EC2.i1PublicIpAddress) runningInstances
-            return ())
-            
+    imageId <- latestImageByName name
+    instance' <- runInstance imageId
+    let instanceId = view EC2.i1InstanceId instance'
+    hoist lift (assignName instanceId name)
+    reservations <- view EC2.dirReservations <$> waitForInstance instanceId
+    let runningInstances = concatMap (^. EC2.rInstances) reservations
+        publicIPs = catMaybes (map (^. EC2.i1PublicIpAddress) runningInstances)
+
+    case publicIPs of
+        []   -> lift (throwError "no IP address returned")
+        [ip] -> return ip
+        _    -> lift (throwError "multiple IP addresses returned")
   where
-    runInstanceRq imageId = EC2.runInstances imageId 1 1
+    waitForInstance instanceId = await EC2.instanceRunning (describeInstanceRq instanceId)
+
+    describeInstanceRq instanceId = EC2.describeInstances
+        & EC2.di1InstanceIds .~ [instanceId]
+
+runInstance :: Text -> AWST (ExceptT Text IO) EC2.Instance
+runInstance imageId = do
+    instances <- view EC2.rirInstances <$> send runInstanceRq
+    case instances of
+        [i] -> return i
+        []  -> lift (throwError "runInstance returned zero instances")
+        _   -> lift (throwError "runInstance returned multiple instances")
+  where
+    runInstanceRq = EC2.runInstances imageId 1 1
         & EC2.riInstanceType ?~ EC2.T2_Medium
 
-    describeInstancesRq instanceIds = EC2.describeInstances
-        & EC2.di1InstanceIds .~ instanceIds
-
-assignName :: [Text] -> Text -> AWST IO ()
+assignName :: Text -> Text -> AWST IO ()
 assignName resourceIds name =
     send_ $ EC2.createTags
-        & EC2.ct1Resources .~ resourceIds
+        & EC2.ct1Resources .~ [resourceIds]
         & EC2.ct1Tags .~ [(EC2.tag "Name" name)]
 
 timestamp :: IO Text
@@ -78,7 +95,7 @@ createImage instanceName = do
             let rs = send (EC2.createImage instanceId $ instanceName <> "-" <> t)
             (view EC2.cirImageId <$> rs) >>= maybe
                 (return $ Left "EC2.createImage did not return an image id.")
-                (\imageId -> do assignName [imageId] instanceName
+                (\imageId -> do assignName imageId instanceName
                                 return $ Right imageId)
         []           -> return $ Left "instance name not found"
         _            -> return $ Left "instance name not unique"
--- src/Run.hs
+++ src/Run.hs
@@ -3,11 +3,13 @@ module Run
     ) where
 
 import           Control.Lens
+import           Control.Monad.Except
 import           Control.Monad.Trans.AWS
+import           Data.Text (Text)
 import           System.IO
 
-run :: (Show a) => AWST IO a -> IO ()
+run :: (Show a) => AWST (ExceptT Text IO) a -> IO (Either Text (Either Error a))
 run f = do
     logger <- newLogger Info stdout
     env <- getEnv Sydney Discover <&> envLogger .~ logger
-    runAWST env f >>= print
+    runExceptT $ runAWST env f
