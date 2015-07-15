1ee2a0517d72025a2e2590798e24b7e79ec7eb4f
lbodor <lazar.bodor@gmail.com>
2015-07-15 10:51:10 +0000

    Read region from an environment variable.

--- src/Run.hs
+++ src/Run.hs
@@ -4,12 +4,15 @@ module Run
 
 import           Control.Lens
 import           Control.Monad.Except
-import           Control.Monad.Trans.AWS
-import           Data.Text (Text)
+import           Control.Monad.Trans.AWS hiding (getEnv)
+import qualified Control.Monad.Trans.AWS as AWS (getEnv)
+import           Data.Text               (Text)
+import           System.Environment      (getEnv)
 import           System.IO
 
 run :: (Show a) => AWST (ExceptT Text IO) a -> IO (Either Text (Either Error a))
 run f = do
     logger <- newLogger Info stdout
-    env <- getEnv Sydney Discover <&> envLogger .~ logger
+    region <- read <$> liftIO (getEnv "AWS_REGION")
+    env <- AWS.getEnv region Discover <&> envLogger .~ logger
     runExceptT $ runAWST env f
