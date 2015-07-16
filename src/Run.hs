cc8b0cd3c0d3b3d7c9587580ecd757f814459b8b
Lazar Bodor <lazar.bodor@ga.gov.au>
2015-07-16 11:31:03 +1000

    Retry all requests 5 times.

--- src/Run.hs
+++ src/Run.hs
@@ -8,6 +8,7 @@ import           Control.Lens
 import           Control.Monad.Except
 import           Control.Monad.Trans.AWS hiding (getEnv)
 import qualified Control.Monad.Trans.AWS as AWS (getEnv)
+import           Control.Retry           (RetryPolicy(..))
 import           Data.Monoid
 import           Data.Text               (Text)
 import qualified Data.Text               as T (pack)
@@ -19,8 +20,13 @@ run :: (Show a) => AWST (ExceptT Text IO) a -> IO ()
 run f = do
     logger <- newLogger Info stdout
     region <- read <$> liftIO (getEnv "AWS_REGION")
-    env    <- AWS.getEnv region Discover <&> envLogger .~ logger
+    env    <- AWS.getEnv region Discover
+                <&> envLogger      .~ logger
+                <&> envRetryPolicy ?~ retryPolicy
     result <- runExceptT $ runAWST env f
     case join (over (_Right . _Left) (T.pack . show) result) of
         Right r -> T.putStrLn ("OK: " <> (T.pack . show) r)
         Left e  -> T.putStrLn ("Error: " <> e)
+  where
+    retryPolicy = RetryPolicy $ \n ->
+      if n < 5 then Just 50000 else Nothing
